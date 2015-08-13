c78955d1e8b7547f005e98e99d446efa726fac54
Amit Ishairzay <amit.ishairzay@gmail.com>
2015-08-13 16:47:36 -0400

    Add mongoose, and add blank css files

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var routes = require(path.join(__dirname, 'backend', 'routes')).init(viewRoot);
 app.set('views', path.join(__dirname, 'frontend', 'views'));
 app.use(express.static(path.join(__dirname, 'frontend', 'public')));
 
+// Database connection
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/item-set-challenge');
+/**
+collections to use:
+items
+  Stores all items given from the Riot API call
+item-sets
+  Stores list of item-sets
+*/
+
 // Routes
 app.get('/item-set/:id', routes.getItemSet);
 app.post('/item-set', routes.createItemSet);
