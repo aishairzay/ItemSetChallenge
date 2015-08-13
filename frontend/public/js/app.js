6d01cacdb8a8e8a4fea1b07f6754c51d8d336bc9
Amit Ishairzay <amit.ishairzay@gmail.com>
2015-08-13 02:03:19 -0400

    Add a crappy navbar

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-
+// Defines app module
 angular.module('app', [
   'app.controllers'
 ]).
-config(function ($routeProvider) {
+config(function ($routeProvider, $locationProvider) {
   $routeProvider
+
+  // Sets routes and
+  // Decides which html file to load up in the main part of the view
   .when('/', {
     templateUrl: 'templates/home.html',
     controller: 'HomeController'
@@ -18,4 +21,10 @@ config(function ($routeProvider) {
   .otherwise({
     redirectTo: '/'
   });
+
+  // not working..?
+  // Changes it so there is no # in the url bar as long as html5 is supported by the browser
+  if(window.history && window.history.pushState) {
+    //$locationProvider.html5Mode(true);
+  }
 });
