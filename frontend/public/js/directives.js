01c5559eeb5b1dd2d6dd569ada5a0ac59fc3e89b
Amit Ishairzay <amit.ishairzay@gmail.com>
2015-08-15 03:07:00 -0400

    attempt popover on items

--- frontend/public/js/directives.js
+++ frontend/public/js/directives.js
@@ -1,18 +1,19 @@
 angular.module('app.directives', []).
 directive('item', function(){
-	return{
+	return {
 		restrict: 'E',
 		scope: {
 			data: '='
 		},
 		templateUrl: 'templates/item-directive.html',
 		controller: function($scope) {
+			$scope.showPopover = false;
       $scope.imageUrl = 'http://ddragon.leagueoflegends.com/cdn/5.15.1/img/item/' + $scope.data.id + '.png';
 		}
 	};
 }).
-directive('block', function(){
-	return{
+directive('block', function() {
+	return {
 		restrict: 'E',
 		scope: {
 			data: '='
